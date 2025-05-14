01e28c1a4587e9c09eba3c7a980364ea0664bb0d
Pranaydeep <hwre2224@gmail.com>
2025-05-14 22:01:13 +0530

    Update lamf_simulator.py

--- lamf_simulator.py
+++ lamf_simulator.py
@@ -195,7 +195,7 @@ input_summary = (
 
 # Add a text box inside the plot (top-left corner)
 ax3.text(
-    0.01, 0.96, input_summary,
+    0.01, 0.98, input_summary,
     transform=ax3.transAxes,
     fontsize=9,
     verticalalignment='top',
