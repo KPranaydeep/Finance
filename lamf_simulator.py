82075905261effc4c45347b6f9d55ff89d26d2c1
Pranaydeep <hwre2224@gmail.com>
2025-05-18 10:14:17 +0530

    Update lamf_simulator.py

--- lamf_simulator.py
+++ lamf_simulator.py
@@ -302,7 +302,7 @@ def format_rupee(x, _=None):
 # Plot using indices for equidistant bars
 indices = np.arange(len(loan_range))
 
-fig, ax = plt.subplots(figsize=(12, 6),dpi=600)
+fig, ax = plt.subplots(figsize=(12, 6),dpi=1000)
 bars = ax.bar(
     indices,
     net_pnl_list,
@@ -314,7 +314,7 @@ bars = ax.bar(
 for i, (bar, value) in enumerate(zip(bars, net_pnl_list)):
     ax.text(
         bar.get_x() + bar.get_width() / 2,
-        value + (3000 if value >= 0 else -3000),
+        value + (2750 if value >= 0 else -2750),
         format_rupee(value, None),
         ha='center', va='bottom' if value >= 0 else 'top',
         fontsize=8, fontweight='bold'
