e20be261a81b5a7144a69234425ba434d6429fa6
Pranaydeep <hwre2224@gmail.com>
2025-06-13 12:15:49 +0530

    Update stocks_performance.py

--- stocks_performance.py
+++ stocks_performance.py
@@ -1,5 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import pandas as pd
 
 # Connect to MongoDB
 uri = "mongodb+srv://hwre2224:jXJxkTNTy4GYx164@finance.le7ka8a.mongodb.net/?retryWrites=true&w=majority&appName=Finance"
@@ -15,3 +16,15 @@ except Exception as e:
 # Define database and collection
 db = client['finance_db']
 collection = db['stock_performance']
+
+# Load data from MongoDB
+data = list(collection.find({}, {"_id": 0}))
+df = pd.DataFrame(data)
+
+# Handle first time empty
+if df.empty:
+    df = pd.DataFrame(columns=['Date', 'Buy', 'Sell', 'Charges'])
+
+# Parse date properly
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+df = df.dropna(subset=['Date'])
