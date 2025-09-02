c37b6496235fcb6eba2e2f488b038812f17b1d81
Pranaydeep <hwre2224@gmail.com>
2025-09-02 12:28:04 +0530

    Update wealthmachine.py

--- wealthmachine.py
+++ wealthmachine.py
@@ -70,7 +70,7 @@ fig.update_traces(
 )
 
 # Streamlit app
-st.subheadear("Portfolio Allocation Visualization")
+st.subheader("Portfolio Allocation Visualization")
 st.plotly_chart(fig, use_container_width=True)
 
 
