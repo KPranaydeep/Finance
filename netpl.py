9dd1c82b8e5ecfba9924cfe7370da404d8439347
Pranaydeep <hwre2224@gmail.com>
2026-04-30 23:02:15 +0530

    Add yfinance import to netpl.py

--- netpl.py
+++ netpl.py
@@ -14,6 +14,7 @@ from datetime import date
 import time
 from matplotlib.colors import ListedColormap
 from matplotlib.ticker import FuncFormatter
+import yfinance as yf
 
 # --- 🧽 Suppress font warnings ---
 warnings.filterwarnings("ignore", category=UserWarning, module="matplotlib.font_manager")
